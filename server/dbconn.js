0f4d45afbbdaf31abba4103009e1ceff863018bc
pierrekasongo <pierrekasongo88@gmail.com>
2019-08-23 12:04:51 +0200

    Using env variables

--- server/dbconn.js
+++ server/dbconn.js
@@ -1,22 +1,19 @@
 const mysql = require('mysql');
-
+require('custom-env').env(true);
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.MYSQL_HOST,
     //port:3306,
-    user: "wpc_user",
-    password: "12345",
-    database: "workforce_pressure",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.DB_NAME,
     multipleStatements:true
     
 });
 connection.connect(function(err) {
-    if (err) console.log("Local database error: ",error);
-    console.log('You are now connected to the local db')
+    if (err) console.log("Local database error: ",err);
+    else
+        console.log('You are now connected to the local db')
 });
-
-
-//let conn = mysql.connect(connection);
-//let conn=connection;
 
 module.exports = connection;
